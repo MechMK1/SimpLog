67d2729606e03f60aa10cb49a2b0f98e380b2deb
MechMK1 <dave.stockinger@gmail.com>
2015-02-10 18:05:03 +0100

    Added public methods to write log

--- MechMK1.SimpLog/MechMK1.SimpLog/Logger.cs
+++ MechMK1.SimpLog/MechMK1.SimpLog/Logger.cs
@@ -9,5 +9,30 @@ namespace MechMK1.SimpLog
     public abstract class Logger
     {
 		abstract protected void Write(string message, LogLevel level);
+
+		public void Debug(string message)
+		{
+			Write(message, LogLevel.Debug);
+		}
+
+		public void Info(string message)
+		{
+			Write(message, LogLevel.Info);
+		}
+
+		public void Warning(string message)
+		{
+			Write(message, LogLevel.Warning);
+		}
+
+		public void Error(string message)
+		{
+			Write(message, LogLevel.Error);
+		}
+
+		public void Fatal(string message)
+		{
+			Write(message, LogLevel.Fatal);
+		}
     }
 }
