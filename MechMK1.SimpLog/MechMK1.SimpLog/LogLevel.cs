f9ea165da630428aed659be3b915d18c5c826052
MechMK1 <dave.stockinger@gmail.com>
2015-02-10 18:03:13 +0100

    Initial creation

---
+++ MechMK1.SimpLog/MechMK1.SimpLog/LogLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechMK1.SimpLog
+{
+	public enum LogLevel
+	{
+		Debug   = 1,
+		Info    = 2,
+		Warning = 3,
+		Error   = 4,
+		Fatal   = 5
+	}
+}
---
+++ MechMK1.SimpLog/MechMK1.SimpLog/Logger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechMK1.SimpLog
+{
+    public abstract class Logger
+    {
+		abstract protected void Write(string message, LogLevel level);
+    }
+}
