e5d6cb90c6397a511777a1f378095d5865352509
MechMK1 <dave.stockinger@gmail.com>
2015-02-12 21:12:30 +0100

    Removed unused usings from Logger

--- MechMK1.SimpLog/MechMK1.SimpLog/Logger.cs
+++ MechMK1.SimpLog/MechMK1.SimpLog/Logger.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MechMK1.SimpLog
+﻿namespace MechMK1.SimpLog
 {
     public abstract class Logger
     {
