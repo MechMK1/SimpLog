7451dcbcbdd38d1532e0caac19a04dce8c3a6048
MechMK1 <dave.stockinger@gmail.com>
2015-02-12 21:54:04 +0100

    Added unit tests

---
+++ MechMK1.SimpLog/MechMK1.SimpLog.Tests/UnitTestConditionalLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MechMK1.SimpLog.Tests
+{
+	[TestClass]
+	public class UnitTestConditionalLogger
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ MechMK1.SimpLog/MechMK1.SimpLog.Tests/UnitTestSilentLogger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MechMK1.SimpLog.Tests
+{
+	[TestClass]
+	public class UnitTestSilentLogger
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
