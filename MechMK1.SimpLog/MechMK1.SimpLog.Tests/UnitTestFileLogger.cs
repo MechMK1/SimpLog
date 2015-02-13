0e7596ecaccef9a8ae3ff190559a84849913f08a
MechMK1 <dave.stockinger@gmail.com>
2015-02-13 14:08:44 +0100

    Added FileLogger and unit tests

---
+++ MechMK1.SimpLog/MechMK1.SimpLog.Tests/UnitTestFileLogger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MechMK1.SimpLog.Tests
+{
+	[TestClass]
+	public class UnitTestFileLogger
+	{
+		static string path = "file.log";
+		static string message = "FooBar";
+		[TestMethod]
+		public void TestMethodReadWrite()
+		{
+			Assert.IsFalse(File.Exists(path));
+			FileLogger l = new FileLogger(path);
+			l.Info(message);
+			string content = File.ReadAllLines(path, l.Encoding)[0];
+			Assert.IsTrue(Regex.IsMatch(content, @"\[.+?\]\ \[\d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2}\.\d{4}\]:\ (.+)"));
+		}
+
+		[TestMethod]
+		public void TestMethodAppend()
+		{
+			Assert.IsFalse(File.Exists(path));
+			File.WriteAllText(path, "Filler" + Environment.NewLine);
+			Assert.IsTrue(File.Exists(path));
+			FileLogger l = new FileLogger(path);
+			l.Info(message);
+			string content = File.ReadAllLines(path, l.Encoding)[1];
+			Assert.IsTrue(Regex.IsMatch(content, @"\[.+?\]\ \[\d{4}-\d{2}-\d{2}\ \d{2}:\d{2}:\d{2}\.\d{4}\]:\ (.+)"));
+		}
+
+		[TestInitialize]
+		[TestCleanup]
+		public void Prepare()
+		{
+			if (File.Exists(path)) File.Delete(path);
+		}
+	}
+}
---
+++ MechMK1.SimpLog/MechMK1.SimpLog/FileLogger.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechMK1.SimpLog
+{
+	public class FileLogger : Logger
+	{
+		public string Path { get; set; }
+		public Encoding Encoding { get; set; }
+
+		public FileLogger(string path) : this(path, defaultEncoding)
+		{
+		}
+
+		public FileLogger(string path, Encoding encoding)
+		{
+			this.Path = path;
+			this.Encoding = encoding;
+		}
+
+		protected internal override void Write(string message, LogLevel level)
+		{
+			File.AppendAllText(
+				this.Path,
+				GetFormattedMessage(message, level),
+				this.Encoding
+			);
+		}
+
+		#region Helpers
+		private static string GetFormattedMessage(string message, LogLevel level)
+		{
+			return string.Format(
+				CultureInfo.InvariantCulture,
+				"[{0}] [{1}]: {2}{3}",
+				GetFormattedLogLevel(level),
+				DateTime.Now.ToString(
+					"yyyy-MM-dd HH:mm:ss.ffff",
+					CultureInfo.InvariantCulture
+				),
+				message,
+				Environment.NewLine
+			);
+		}
+
+		private static string GetFormattedLogLevel(LogLevel level)
+		{
+			switch (level)
+			{
+				case LogLevel.Debug:
+				case LogLevel.Info:
+				case LogLevel.Warning:
+				case LogLevel.Error:
+					return level.ToString().ToUpperInvariant();
+				case LogLevel.Fatal:
+					return "--- F A T A L ---";
+				default:
+					throw new ArgumentOutOfRangeException("level");
+			}
+		}
+		#endregion Helpers
+
+		private static Encoding defaultEncoding = Encoding.UTF8;
+	}
+}
